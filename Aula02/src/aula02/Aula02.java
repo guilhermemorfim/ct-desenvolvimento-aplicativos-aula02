277f039d8087b4cf2cd9ff5c99e566c145fd099f
guilhermemorfim <g.morfim@outlook.com>
2020-11-09 21:38:19 -0300

    Commit inicial

---
+++ Aula02/src/aula02/Aula02.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula02;
+
+import model.Pessoa;
+
+/**
+ *
+ * @author SESI_SENAI
+ */
+public class Aula02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Pessoa p1 = new Pessoa("Guilherme", 30, 1.74);
+        Pessoa p2 = p1;
+
+    }
+    
+}
---
+++ Aula02/src/model/Pessoa.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author SESI_SENAI
+ */
+public class Pessoa {
+    
+    private String nome;
+    private int idade;
+    private double altura;
+
+    public Pessoa(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    
+    
+    
+}
